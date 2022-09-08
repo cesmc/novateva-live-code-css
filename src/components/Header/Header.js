f46cd768dc02dbda796d1e9103ab232070bd506a
cesmc <xccuevas@gmail.com>
2022-09-08 11:26:20 -0500

    feat(header): create header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Nueva Tabla</h1>
+      <button className="header_button">Guardar</button>
+    </div>
+  );
+};
+
+export default Header;
